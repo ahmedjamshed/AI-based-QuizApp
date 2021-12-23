b072cca386155a99356d5571e094e5ca3176e2cb
Ahmed Jamshed <ahmedjamshedlead@gmail.com>
2021-12-23 18:08:29 +0500

    google storage

--- lib/app/common/constants.dart
+++ lib/app/common/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   const Constants._();
 
-  static const String BASE_URL = 'http://12e7-35-232-26-158.ngrok.io/';
+  static const String BASE_URL = 'http://ac2a-34-140-115-7.ngrok.io/';
 
   static const timeout = Duration(seconds: 30);
   static const String TOKEN = 'authToken';
