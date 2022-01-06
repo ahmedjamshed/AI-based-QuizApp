e83e441f3fd5c62734dceceb861f6223461a56a6
Ahmed Jamshed <ahmedjamshedlead@gmail.com>
2022-01-06 19:07:21 +0500

    ipynbs

--- lib/app/common/constants.dart
+++ lib/app/common/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   const Constants._();
 
-  static const String BASE_URL = 'http://0e28-35-238-68-228.ngrok.io/';
+  static const String BASE_URL = 'http://6adc-35-188-92-19.ngrok.io/';
 
   static const timeout = Duration(seconds: 30);
   static const String TOKEN = 'authToken';
