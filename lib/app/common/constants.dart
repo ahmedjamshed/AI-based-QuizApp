9a67236002359200247cd7b8e76bf8fac2f9090e
Ahmed <ahmedjamshed3@gmail.com>
2022-01-21 03:12:49 +0500

    update

--- lib/app/common/constants.dart
+++ lib/app/common/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   const Constants._();
 
-  static const String BASE_URL = 'http://3abe-35-224-211-66.ngrok.io/';
+  static const String BASE_URL = 'http://7070-34-136-91-23.ngrok.io/';
 
   static const timeout = Duration(seconds: 60 * 2);
   static const String TOKEN = 'authToken';
